f513f8dee8ba05676178e7c40afeb56f9662f232
gael <grenalt3424@gmail.com>
2021-03-29 00:41:23 +0200

    update startup file

--- githubActionTest/Startup.cs
+++ githubActionTest/Startup.cs
@@ -40,11 +40,11 @@ namespace githubActionTest
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "githubActionTest v1"));
+                
             }
-
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "githubActionTest v1"));
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
